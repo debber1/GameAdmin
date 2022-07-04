8cb9da3e6e2c52f5c063f92d4f4035271b8934a4
Robbe Decapmaker <robbe.decapmaker@hotmail.be>
2022-07-04 14:08:53 +0200

    Adding settings page

---
+++ gameadmin/lib/ui/pages/settings.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gameadmin/ui/pages/scoreboard.dart';
+
+import '../../cubit/scoreboard_cubit.dart';
+
+class Settings extends StatelessWidget {
+  Settings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+    return MaterialApp(
+      title: 'GameAdmin',
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(35.0),
+          child: AppBar(
+            title: Text('GameAdmin - github.com/debber1/GameAdmin'),
+          ),
+        ),
+        body: Container(
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(),
+              ),
+              Expanded(
+                flex: 50,
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.7),
+                      borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(10),
+                          right: Radius.circular(10)),
+                    ),
+                    child: Center(
+                      child: Text("Without Tournament Organiser"),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(),
+              ),
+              Expanded(
+                flex: 50,
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.7),
+                      borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(10),
+                          right: Radius.circular(10)),
+                    ),
+                    child: Center(
+                      child: Text("With Tournament Organiser"),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- gameadmin/lib/ui/router.dart
+++ gameadmin/lib/ui/router.dart
@@ -3,16 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gameadmin/cubit/scoreboard_cubit.dart';
 import 'package:gameadmin/ui/pages/scoreboard.dart';
+import 'package:gameadmin/ui/pages/settings.dart';
 
 class GameAdminRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case "/":
-        return MaterialPageRoute(
-            builder: (_) => BlocProvider(
-                  create: (BuildContext context) => ScoreboardCubit(),
-                  child: ScoreBoard(),
-                ));
+        return MaterialPageRoute(builder: (_) => Settings());
       default:
         return null;
     }
